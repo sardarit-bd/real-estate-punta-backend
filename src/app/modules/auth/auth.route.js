1573ef986eb5ecce5250223c2934870d62c156f7
Md <sohelf131@gmail.com>
2025-12-05 16:23:29 +0600

    added get me route

--- src/app/modules/auth/auth.route.js
+++ src/app/modules/auth/auth.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { AuthControllers } from "./auth.controller.js";
+import { checkAuth } from "../../middlewares/checkAuth.js";
 
 
 const router = Router();
 
 router.post("/register", AuthControllers.createUser);
-router.get('/me', AuthControllers.getMe)
+router.get('/me', checkAuth('owner'), AuthControllers.getMe)
 router.post("/login", AuthControllers.credentialsLogin);
 router.post("/logout", AuthControllers.logout);
 
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: ['https://coffee-mocha-chi.vercel.app']
+  origin: ['https://real-estate-punta.vercel.app']
 }))
 
 app.use("/api", router);
