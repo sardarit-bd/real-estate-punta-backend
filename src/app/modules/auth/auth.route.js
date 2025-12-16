10792193c0fde7e2654013c2faf2ef632bd975e4
Sohel <sohelf131@gmail.com>
2025-12-16 13:20:06 +0600

    protected route

--- src/app/modules/auth/auth.route.js
+++ src/app/modules/auth/auth.route.js
@@ -7,15 +7,15 @@ import { Role } from "./auth.model.js";
 const router = Router();
 
 router.post("/register", AuthControllers.createUser);
-router.get('/me', checkAuth('owner', 'super_admin'), AuthControllers.getMe)
+router.get('/me', checkAuth(Role.OWNER, Role.TENANT, Role.ADMIN, Role.SUPER_ADMIN), AuthControllers.getMe)
 router.delete('/delete-me', checkAuth(Role.OWNER, Role.TENANT, Role.ADMIN), AuthControllers.deleteMe)
+
 router.post("/login", AuthControllers.credentialsLogin);
 router.post("/logout", AuthControllers.logout);
 
-router.post("/change-password", AuthControllers.changePassword);
-
+router.post("/change-password", checkAuth(Role.OWNER, Role.TENANT, Role.ADMIN, Role.SUPER_ADMIN), AuthControllers.changePassword);
 router.post("/forgot-password", AuthControllers.forgotPassword);
-router.post("/reset-password", checkAuth('owner', 'super_admin', Role.TENANT), AuthControllers.resetPassword);
+router.post("/reset-password", checkAuth(Role.OWNER, Role.TENANT, Role.ADMIN, Role.SUPER_ADMIN), AuthControllers.resetPassword);
 
 
 export const AuthRoutes = router;
